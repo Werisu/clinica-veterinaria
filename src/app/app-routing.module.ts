aada0c1563002243c1ac2a0d745fac6b1868c8fd
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-11-21 17:43:02 -0300

    criações de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'painel',
+    loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
+    data: {
+      breadcrumb: {
+        label: 'Inicio',
+        info: 'home'
+      }
+    }
+  },
+  { path: '**', redirectTo: 'painel', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './../../core/core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CoreModule
   ]
 })
 export class HomeModule { }
