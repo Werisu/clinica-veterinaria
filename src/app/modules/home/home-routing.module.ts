0abd4159473003e062be9cfd560ff88971c68b75
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-12-22 22:01:40 -0300

    modified

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -5,9 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: HomeComponent, children: [
     { path: 'inicio', loadChildren: () => import('./pages/dashboard/dashboard.module').then((m) => m.DashboardModule) },
-    { path: 'usuarios', loadChildren: () => import('./pages/user-registration/user-registration.module').then((m) => m.UserRegistrationModule) },
+    { path: 'usuarios', loadChildren: () => import('./pages/users/user-registration.module').then((m) => m.UserRegistrationModule) },
     { path: 'proprietarios', loadChildren: () => import('./pages/owners/owners.module').then((m) => m.OwnersModule) },
-    { path: 'pacientes', loadChildren: () => import('./pages/patient-record/patient-record.module').then((m) => m.PatientRecordModule) },
+    { path: 'pacientes', loadChildren: () => import('./pages/patients/patient-record.module').then((m) => m.PatientRecordModule) },
     { path: '', redirectTo: '/painel/inicio', pathMatch: 'full' }
   ] }
 ];
